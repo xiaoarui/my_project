4aafe1b84add1a7f7d9250dea88e2dc488fe0ce1
肖瑞 <xiaorui@ecmoban.com>
2019-09-10 14:00:54 +0800

    提交测试

---
+++ src/Project/ProjectClass/Tools.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Project\ProjectClass;
+/**
+ * Class demoSrc
+ */
+class Tools{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function demo($result = 'function demo'){
+        echo $result;
+    }
+
+
+    public function test($result = 'function test'){
+        echo $result;
+    }
+
+}
---
+++ src/Wap/NewTools.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wap;
+/**
+ * Class demoSrc
+ */
+class NewTools{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function demoWap(){
+        echo 'hello demoWap';
+    }
+
+
+    public function testWap(){
+        echo 'hello testWap';
+    }
+
+}
